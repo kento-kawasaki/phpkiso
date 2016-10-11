a5568d473dede25963a2c7413ded7081c7c9db24
kento-kawasaki <13e0222@gmail.com>
2016-10-11 11:40:46 +0800

    変更

---
+++ search.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>検索ページ</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <form action="" method="post">
+    <p>検索したいcodeを入力してください。</p>
+    <input type="text" name="code">
+    <input type="submit" value="検索">
+  </form>
+</body>
+</html>
